6a9e8a2ae44f28a5b92270b31cb0d9025cc7b371
nahuelangelone <nahuelangelone94@gmail.com>
2024-02-01 01:03:57 -0300

    implementacion del css nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import './App.css';
 import Navbar from "./components/NavBar/NavBar";
 import ProfileCard from "./components/Cards/ProfileCard";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import CharizardImg from "./Img/Charizard.png"
 import BlastoiseImg from "./Img/Blastoise.png"
 import VenusaurImg from "./Img/Venusaur.png"
@@ -7,9 +9,10 @@ import VenusaurImg from "./Img/Venusaur.png"
 
 function App(){
     return (
-						<div>
+						<div className="App">
 							<Navbar/>
-							<h1>Pokémon</h1>
+							<ItemListContainer greeting={'Hola, bienvenido a'}/>
+							<h1>Pokémon Store</h1>
 
 							<ProfileCard titulo="Charizard" tipo="Fuego" img={CharizardImg}/>
 							<ProfileCard titulo="Blastoise" tipo="Agua" img={BlastoiseImg}/>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,18 @@
+
+import './NavBar.css'
 import CartWidget from "../CartWidget/CartWidget"
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <div>EurEk4rds</div>
-                <div>
+        <nav className="Navbar">
+            <div className="Navbar-container">
+                <div className="Navbar-logo">EurEk4rds</div>
+                <div className="Navbar-buttons">
                     <button>Cartas</button>
                     <button>Tins</button>
                     <button>Box</button>
                 </div>
             </div>
-            <CartWidget/>
+            <CartWidget />
         </nav>
     )
 }
